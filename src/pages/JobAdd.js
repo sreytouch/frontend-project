678caf42bbddbb1c762f04b92c3e17e415bf23ce
sreytouch <atouch.it@gmail.com>
2022-07-10 13:51:17 -0500

    add job

--- src/pages/JobAdd.js
+++ src/pages/JobAdd.js
@@ -64,7 +64,7 @@ const AddButton = props => {
 
 const pagination = paginationFactory({
     page: 1,
-    sizePerPage: 10,
+    sizePerPage: 5,
     lastPageText: '>>',
     firstPageText: '<<',
     nextPageText: '>',
@@ -82,12 +82,51 @@ const pagination = paginationFactory({
 });
 
 const JobAdd = () => {
-    const linkFollow = (cell, row, rowIndex, formatExtraData) => {
+
+    const products = [
+        { id: 1, description: 'George', benifit: 'Monkey', creator: 'qq', tags: ['hello1', 'hello2', 'hello3'], fileName: ['2211', '2211'] },
+        { id: 2, description: 'Jeffrey', benifit: 'Giraffe', creator: 'ww', tags: ['test1', 'test2', 'test3'], fileName: ['3333', '3333'] },
+        { id: 3, description: 'Alice', benifit: 'Giraffe', creator: 'ee', tags: ['new1', 'new2', 'new3'], fileName: ['1221', '1221'] },
+        { id: 4, description: 'Foster', benifit: 'Tiger', creator: 'rr', tags: ['jessica', 'jessica2', 'jessica3'], fileName: ['3443', '3443'] },
+        { id: 5, description: 'Tracy', benifit: 'Bear', creator: 'tt', tags: ['hely1', 'hely2', 'hely3'], fileName: ['5554', '5554'] },
+        { id: 6, description: 'Joesph', benifit: 'Lion', creator: 'yy', tags: ['loyo'], fileName: ['5656'] },
+        { id: 7, description: 'Tania', benifit: 'Deer', creator: 'yy', tags: ['deol'], fileName: ['7666'] },
+        { id: 8, description: 'Chelsea', benifit: 'Tiger', creator: 'uu', tags: ['tede'], fileName: ['7878'] },
+        { id: 9, description: 'Benedict', benifit: 'Tiger', creator: 'ii', tags: ['tepo'], fileName: ['8777'] },
+        { id: 10, description: 'Chadd', benifit: 'Lion', creator: 'oo', tags: ['nono'], fileName: ['8899'] },
+        { id: 11, description: 'Delphine', benifit: 'Deer', creator: 'pp', tags: ['momo'], fileName: ['7766'] },
+        { id: 12, description: 'Elinore', benifit: 'Bear', creator: 'll', tags: ['lolo'], fileName: ['9090'] },
+        { id: 13, description: 'Stokes', benifit: 'Tiger', creator: 'kk', tags: ['yoyo'], fileName: ['9999'] },
+        { id: 14, description: 'Tamara', benifit: 'Lion', creator: 'jj', tags: ['gogo'], fileName: ['9898'] },
+        { id: 15, description: 'Zackery', benifit: 'Bear', creator: 'hh', tags: ['dodo'], fileName: ['9666'] }
+    ];
+
+    const ApplyBtn = (cell, row, rowIndex, formatExtraData) => {
         return (
             <div>
-                <Button> Apply </Button> &nbsp; 
+                <Button> Apply </Button> &nbsp;
                 {/* <Button> Edit </Button> */}
-                <EditJob/> 
+                <EditJob />
+            </div>
+        );
+    };
+
+    const TagP = (cell, row, rowIndex, formatExtraData) => {
+        return (
+            <div>
+                {row.tags.map((rowIndex, key) => (
+                    <p key={key}>{rowIndex}</p>
+                ))}
+            </div>
+        );
+    };
+
+    const FileN = (cell, row, rowIndex, formatExtraData) => {
+        return (
+            <div>
+                {row.fileName.map((rowIndex, key) => (
+                    <p key={key}>{rowIndex}</p>
+                ))}
             </div>
         );
     };
@@ -137,6 +176,7 @@ const JobAdd = () => {
                     tagsFilter = filter;
                 }
             }),
+            formatter: TagP,
             sort: true
         },
         {
@@ -147,12 +187,13 @@ const JobAdd = () => {
                     fileName = filter;
                 }
             }),
+            formatter: FileN,
             sort: true
         },
         {
             dataField: "action",
             text: "Action:",
-            formatter: linkFollow
+            formatter: ApplyBtn
         }
     ];
 
@@ -164,41 +205,21 @@ const JobAdd = () => {
         fileName("");
     }
 
-    const products = [
-        { id: 1, description: 'George', benifit: 'Monkey', creator: 'qq', tags: ['hello1', 'hello2', 'hello3'], fileName: ['2211', '2211'] },
-        { id: 2, description: 'Jeffrey', benifit: 'Giraffe', creator: 'ww', tags: ['test1', 'test2', 'test3'], fileName: ['3333', '3333'] },
-        { id: 3, description: 'Alice', benifit: 'Giraffe', creator: 'ee', tags: ['new1', 'new2', 'new3'], fileName: ['1221', '1221'] },
-        { id: 4, description: 'Foster', benifit: 'Tiger', creator: 'rr', tags: ['jessica', 'jessica2', 'jessica3'], fileName: ['3443', '3443'] },
-        { id: 5, description: 'Tracy', benifit: 'Bear', creator: 'tt', tags: ['hely1', 'hely2', 'hely3'], fileName: ['5554', '5554'] },
-        { id: 6, description: 'Joesph', benifit: 'Lion', creator: 'yy', tags: ['loyo'], fileName: ['5656'] },
-        { id: 7, description: 'Tania', benifit: 'Deer', creator: 'yy', tags: ['deol'], fileName: ['7666'] },
-        { id: 8, description: 'Chelsea', benifit: 'Tiger', creator: 'uu', tags: ['tede'], fileName: ['7878'] },
-        { id: 9, description: 'Benedict', benifit: 'Tiger', creator: 'ii', tags: ['tepo'], fileName: ['8777'] },
-        { id: 10, description: 'Chadd', benifit: 'Lion', creator: 'oo', tags: ['nono'], fileName: ['8899'] },
-        { id: 11, description: 'Delphine', benifit: 'Deer', creator: 'pp', tags: ['momo'], fileName: ['7766'] },
-        { id: 12, description: 'Elinore', benifit: 'Bear', creator: 'll', tags: ['lolo'], fileName: ['9090'] },
-        { id: 13, description: 'Stokes', benifit: 'Tiger', creator: 'kk', tags: ['yoyo'], fileName: ['9999'] },
-        { id: 14, description: 'Tamara', benifit: 'Lion', creator: 'jj', tags: ['gogo'], fileName: ['9898'] },
-        { id: 15, description: 'Zackery', benifit: 'Bear', creator: 'hh', tags: ['dodo'], fileName: ['9666'] }
-    ];
-
     const rowClasses = (row, rowIndex) => {
         let h;
-        if(row.tags.length>=2){
+        if (row.tags.length >= 2) {
             h = 'custom-row-class';
         }
         return h;
     };
     const rowStyle = (row, rowIndex) => {
-        console.log("--rowrowrow--", row.tags);
+        // console.log("--rowrowrow--", row.tags);
         let h;
-        if(row.tags.length>=2){
-            h =  { backgroundColor: 'red' };
+        if (row.tags.length >= 2) {
+            h = { backgroundColor: 'red' };
         }
         return h;
     };
-
-    //   console.log("--products--", products[0].fileName.length>=2);
 
     return (
         <div>
@@ -211,7 +232,6 @@ const JobAdd = () => {
             >
                 {props => (
                     <div>
-                    {console.log("==props==",props.baseProps.columns[3])}
                         <SearchBar
                             {...props.searchProps}
                             style={{ width: "400px", height: "40px" }}
@@ -221,7 +241,7 @@ const JobAdd = () => {
                             clearAllFilter={clearAllFilter}
                         />
                         {/* <AddButton/> */}
-                        <AddJob/>
+                        <AddJob />
                         <BootstrapTable
                             {...props.baseProps}
                             filter={filterFactory()}
@@ -230,8 +250,8 @@ const JobAdd = () => {
                             striped
                             hover
                             condensed
-                            rowClasses={ rowClasses }
-                            rowStyle={ rowStyle }
+                            // rowClasses={rowClasses}
+                            // rowStyle={rowStyle}
                             // rowStyle={{ backgroundColor: 'red' }}
                         />
                         {/* <Table striped bordered hover responsive="md">
